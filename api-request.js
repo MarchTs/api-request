6408eb35f967d6ecaafb378502f21f7c0f0b3761
Thanapat Saisoontornwattana <march_ts@outlook.com>
2019-03-29 12:50:29 +0700

    public 1.0.4

--- api-request.js
+++ api-request.js
@@ -17,3 +17,19 @@ module.exports = async (url, httpOption = HttpOption, requestBody) => {
     }
     return result;
 };
+
+module.exports.info = async (url, httpOption = HttpOption, requestBody) => {
+    if (typeof url === "string") {
+        url = new URL(url);
+    }
+    let result = await httpRequest(url, httpOption, requestBody);
+
+    let user_agent = httpOption.option.headers["User-Agent"];
+    logManager.insert3Party(url, requestBody, user_agent, result);
+
+    if (!result.statusCode) {
+        console.log("changeing status code at url:", url.toString());
+        result.statusCode = 500;
+    }
+    return result;
+};
